44a2d8b58b859e5ec42dbe04aefceccaf2a0fd3d
Hellkid137 <Elan.lima10@outlook.com>
2025-09-12 00:19:23 -0300

    correção da chave do banco de dados

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
 
-const supabaseUrl = 'https://tmxtxwvpyrnoqpeyqyqx.supabase.co'; // substitua pela sua URL
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRteHR4d3ZweXJub3FwZXlxeXF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg5OTkwMTQsImV4cCI6MjA2NDU3NTAxNH0.TDR9pJFn6eN4hT86WKGjpk14_lXeRPH5msGVmgFsXtI'; // substitua pela sua chave 
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
